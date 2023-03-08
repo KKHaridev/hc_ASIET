a694de79bc046e02ce60f1ce645a3759a2154607
aaronvincent6411 <aaronvincent6411@gmail.com>
2023-03-08 15:52:51 +0530

    Updated sections

--- sections/1-Main/Main.jsx
+++ sections/1-Main/Main.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React,{useEffect,useRef} from 'react';
 import style from "./Main.module.css";
 import heroimg from "../../assets/heroimg.png"
 import Image from 'next/image';
+import Typed from "typed.js";
+
 
 export const Main = () => {
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+        strings: ["Hack Club ASIET"], 
+        startDelay: 300,
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 100,
+        loop: false,
+    });
+    return () => {
+        typed.destroy();
+    };
+}, []);
+const el = useRef(null);
   return (
     <div className={style.main}>
       <div className={"container" + ' ' + style.box}>
         <div className={style.contentholder}>
           <div className={style.content}>
-            <h1 className="h1">Create Technology for the Greater Good </h1>
+            <h1 className="h1"><span ref={el}></span> </h1>
             <h3 className={"heading"}>Hack Club is a community for young eager learners and coders
               to collaborate, build and create innovative projects and ideas. It
               is a wonderful platform for youngsters to get to know each
--- sections/4-Events/Events.jsx
+++ sections/4-Events/Events.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 import style from "./Events.module.css";
 import Image from 'next/image';
-import img1 from "../../assets/ai.jpeg";
-import img2 from "../../assets/Web.jpeg";
-import img4 from "../../assets/read.jpeg";
-import img3 from "../../assets/python.jpeg";
-import img5 from "../../assets/productdesign.jpeg";
+import { events } from './data';
+import { Router, useRouter } from 'next/router';
+import { HiChevronRight } from 'react-icons/hi';
+import Link from 'next/link';
+
 
 
 
 
 
 export const Events = () => {
+  const router = useRouter();
   return (
     <div className={style.events}>
       <div className={"container"}>
 
         <h1 className="h1">Events</h1>
         <div className={style.box}>
-          <div className={style.big}>
-            <Image src={img1} alt="Events" placeholder="blur" blurDataURL={img1} /> 
+          {events.slice(0,4).map((item,index)=>item.id===0?<div className={style.big}>
+            <img src={item.imgUrl} alt="Events" placeholder="blur" blurDataURL={item.imgUrl} /> 
           </div>
+          :
           <div className={style.small}>
-            <Image src={img2} alt="Events" placeholder="blur" blurDataURL={img2} /> 
+            <img src={item.imgUrl} alt="Events" placeholder="blur" blurDataURL={item.imgUrl} /> 
+          </div>)}
+          <Link href="/events#events" scroll={false}>
+          <div className={style.viewmore}>
+            <div className={style.more}>
+                <h2>Know More <HiChevronRight/></h2>
+            </div>
           </div>
-          <div className={style.small}>
-            <Image src={img3} alt="Events" placeholder="blur" blurDataURL={img3} /> 
-          </div>
-          <div className={style.small}>
-            <Image src={img4} alt="Events" placeholder="blur" blurDataURL={img4} /> 
-          </div>
-          <div className={style.small}>
-            <Image src={img5} alt="Events" placeholder="blur" blurDataURL={img5} /> 
-          </div>
+          </Link>
         </div>
       </div>
     </div>
