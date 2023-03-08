ca6fd7b50e5a9d4a7f583093f5cfb0820ed647ff
haridev <haridev@root533.premium-rootserver.net>
2023-03-08 22:21:36 +0530

    Updated key values

--- pages/events.js
+++ pages/events.js
@@ -1,17 +1,10 @@
 
-import React,{useEffect} from 'react'
+import React from 'react'
 import { events as _events } from '../sections/4-Events/data'
 import style from "../styles/Events.module.css";
 
 export default function events() {
-  useEffect(() => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
-}, []);
-  
+
   return (
     <div className={style.events} id="events">
       <div className={"container"}>
@@ -19,8 +12,8 @@ export default function events() {
         <h1 className="h1">Events</h1>
         <div className={style.box}>
           {_events.map((item,index)=>
-          <div className={style.small}>
-            <img src={item.imgUrl} alt="Events" placeholder="blur" blurDataURL={item.imgUrl} /> 
+          <div className={style.small} key={index}>
+            <img src={item.imgUrl} alt={item.name} /> 
           </div>)}
         </div>
       </div>
--- sections/4-Events/Events.jsx
+++ sections/4-Events/Events.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
+import React from "react";
 import style from "./Events.module.css";
-import Image from 'next/image';
-import { events } from './data';
-import { Router, useRouter } from 'next/router';
-import { HiChevronRight } from 'react-icons/hi';
-import Link from 'next/link';
-
-
-
-
-
+import Image from "next/image";
+import { events } from "./data";
+import { Router, useRouter } from "next/router";
+import { HiChevronRight } from "react-icons/hi";
+import Link from "next/link";
 
 export const Events = () => {
   const router = useRouter();
   return (
     <div className={style.events}>
       <div className={"container"}>
-
         <h1 className="h1">Events</h1>
         <div className={style.box}>
-          {events.slice(0,4).map((item,index)=>item.id===0?<div className={style.big}>
-            <img src={item.imgUrl} alt="Events" placeholder="blur" blurDataURL={item.imgUrl} /> 
-          </div>
-          :
-          <div className={style.small}>
-            <img src={item.imgUrl} alt="Events" placeholder="blur" blurDataURL={item.imgUrl} /> 
-          </div>)}
+          {events.slice(0, 4).map((item, index) =>
+            item.id === 0 ? (
+              <div className={style.big} key={index}>
+                <img src={item.imgUrl} alt={item.name} />
+              </div>
+            ) : (
+              <div className={style.small} key={index}>
+                <img src={item.imgUrl} alt={item.name} />
+              </div>
+            )
+          )}
           <Link href="/events#events" scroll={false}>
-          <div className={style.viewmore}>
-            <div className={style.more}>
-                <h2>Know More <HiChevronRight/></h2>
+            <div className={style.viewmore}>
+              <div className={style.more}>
+                <h2>
+                  Know More <HiChevronRight />
+                </h2>
+              </div>
             </div>
-          </div>
           </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
