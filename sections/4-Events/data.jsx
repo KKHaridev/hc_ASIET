1d211c721d6fe51e84579df2205e09e6ee9102ff
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-03-08 13:41:17 -0500

    Added full events

--- sections/4-Events/data.jsx
+++ sections/4-Events/data.jsx
@@ -7,75 +7,125 @@ export const events=[
     {
       id:1,
       imgUrl:"https://live.staticflickr.com/65535/52734099593_b137f2d736_b.jpg",
-      name:""
+      name:"Git Workshop"
     },
     {
       id:2,
       imgUrl:"https://live.staticflickr.com/65535/52733615291_cabc756668_c.jpg",
-      name:""
+      name:"Python Basics Workshop"
     },
     {
       id:3,
       imgUrl:"https://live.staticflickr.com/65535/52734099633_c8f75b0bc2_c.jpg",
-      name:""
+      name:"AMA Session"
     },
     {
       id:4,
       imgUrl:"https://live.staticflickr.com/65535/52733668701_e4542dd5ff_b.jpg",
-      name:""
+      name:"Heck Teck"
     },
     {
       id:5,
       imgUrl:"https://live.staticflickr.com/65535/52734082385_295e552669_b.jpg",
-      name:""
+      name:"Python For Machine Learning"
     },
     {
       id:6,
       imgUrl:"https://live.staticflickr.com/65535/52734152263_476b972a34_b.jpg",
-      name:""
+      name:"Web Development"
     },
     {
       id:7,
       imgUrl:"https://live.staticflickr.com/65535/52733930187_39fe356b97_b.jpg",
-      name:""
+      name:"Majestic Bibliophile"
     },
     {
       id:8,
       imgUrl:"https://live.staticflickr.com/65535/52734152228_6af4641bc3_c.jpg",
-      name:""
+      name:"Cyber Security"
     },
     {
       id:9,
       imgUrl:"https://live.staticflickr.com/65535/52733668691_a9d45f6998_c.jpg",
-      name:""
+      name:"Mother's Day"
     },
     {
       id:10,
       imgUrl:"https://live.staticflickr.com/65535/52733922824_153e1872d9_c.jpg",
-      name:""
+      name:"Web Development"
     },{
       id:11,
       imgUrl:"https://live.staticflickr.com/65535/52733668506_19d135bbe1_c.jpg",
-      name:""
+      name:"Product Design and Development"
     },
     {
       id:12,
       imgUrl:"https://live.staticflickr.com/65535/52733668461_12d3b5fd78_c.jpg",
-      name:""
+      name:"Eid Mubarak"
     },
     {
       id:13,
       imgUrl:"https://live.staticflickr.com/65535/52734082225_b4b5ea55a0_c.jpg",
-      name:""
+      name:"Teacher's Day"
     },
     {
       id:14,
       imgUrl:"https://live.staticflickr.com/65535/52733144972_7df8c0fc07_c.jpg",
-      name:""
+      name:"Independence Day"
     },
     {
       id:15,
       imgUrl:"https://live.staticflickr.com/65535/52734152073_107d67c405_c.jpg",
-      name:""
+      name:"Onam"
+    },
+    {
+      id:16,
+      imgUrl:"https://live.staticflickr.com/65535/52733922929_491a0c7e1b_c.jpg",
+      name:"Peace Day"
+    },
+    {
+      id:17,
+      imgUrl:"https://live.staticflickr.com/65535/52734152083_5db6157498_c.jpg",
+      name:"Hack Your Way"
+    },
+    {
+      id:18,
+      imgUrl:"https://live.staticflickr.com/65535/52734510701_579699b50e_c.jpg",
+      name:"Buzz Logic"
+    },
+    {
+      id:19,
+      imgUrl:"https://live.staticflickr.com/65535/52734152023_9b6f478ae3_c.jpg",
+      name:"Fact Profile"
+    },
+    {
+      id:20,
+      imgUrl:"https://live.staticflickr.com/65535/52733995217_89636b5f09_c.jpg",
+      name:"Gist Of Git"
+    },
+    {
+      id:21,
+      imgUrl:"https://live.staticflickr.com/65535/52733144917_a565f5d9dc_c.jpg",
+      name:"Kerala Piravi"
+    },
+    {
+      id:22,
+      imgUrl:"https://live.staticflickr.com/65535/52734948835_209fdb46cd_c.jpg",
+      name:"Design Principles"
+    },
+    {
+      id:23,
+      imgUrl:"https://live.staticflickr.com/65535/52733922954_ee26752af2_c.jpg",
+      name:"Type Racer"
+    },
+    {
+      id:24,
+      imgUrl:"https://live.staticflickr.com/65535/52734082290_3b55f3c865_c.jpg",
+      name:"Arduino Workshop"
+    },
+    {
+      id:25,
+      imgUrl:"https://live.staticflickr.com/65535/52733922999_ff33f9e2f7_c.jpg",
+      name:"Republic Day"
     }
   ]
