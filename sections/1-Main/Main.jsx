7be5fafd5cd1d347086c8e585b75e15066529d5b
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-03-19 05:46:07 +0000

    some changes in header and automation

--- sections/1-Main/Main.jsx
+++ sections/1-Main/Main.jsx
@@ -8,8 +8,8 @@ import Typed from "typed.js";
 export const Main = () => {
   useEffect(() => {
     const typed = new Typed(el.current, {
-        strings: ["Welcome","to","official page of","Hack Club ASIET"], 
-        startDelay: 300,
+        strings: ["Welcome","To","Official Page", "Of","Hack Club ASIET"], 
+        startDelay: 200,
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 100,
