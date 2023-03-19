646421252f1936a20fc8bc7390743aea1034d723
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2023-03-19 05:53:33 +0000

    some changes in img of contributors 2

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -55,10 +55,10 @@ export const Footer = () => {
           <h3>Contributors</h3>
           <div className={style.contributors}>
             <a href='https://github.com/KKHaridev'>
-              <img src="https://avatars.githubusercontent.com/u/67409998?v=4"/>
+              <img src="https://avatars.githubusercontent.com/u/67409998?v=4"  height="10" width="10"/>
             </a>
             <a href='https://github.com/AaronVincent6411'>
-              <img src="https://avatars.githubusercontent.com/u/90915273?v=4"/>
+              <img src="https://avatars.githubusercontent.com/u/90915273?v=4"  height="10" width="10"/>
             </a>
         </div>
         </div>
